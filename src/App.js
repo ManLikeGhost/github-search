14a65def773496f56a44bfe32533166b5179c386
manlikeghost <ekpoabasiama@gmail.com>
2022-01-09 15:39:43 +0100

    router included and ui updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
 import NavBar from './components/layout/NavBar';
 import Users from './components/users/Users';
-import Filter from './components/users/Filter'
+import Filter from './components/users/Filter';
 import axios from 'axios';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About'
+import './App.css';
 class App extends Component {
 	state = {
 		users: [],
 		loading: false,
-		alert: null
+		alert: null,
 	};
 
 	async componentDidMount() {
@@ -22,38 +24,60 @@ class App extends Component {
 		this.setState({ users: res.data, loading: false });
 	}
 
-	filterUsers = async ( text ) => {
-		this.setState({ loading:true })
+	filterUsers = async (text) => {
+		this.setState({ loading: true });
 
 		const res = await axios.get(
 			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
 
 		this.setState({ users: res.data.items, loading: false });
-	}
+	};
 
-	clearFilteredUsers = () => {this.setState({ users:[], loading: false})}
+	clearFilteredUsers = () => {
+		this.setState({ users: [], loading: false });
+	};
 
-	setAlert = ( msg, type ) => {
-		this.setState( {
+	setAlert = (msg, type) => {
+		this.setState({
 			alert: {
 				msg,
-				type
-			}
-		} )
-		setTimeout(() => this.setState({ alert: null }), 5000)
-	}
+				type,
+			},
+		});
+		setTimeout(() => this.setState({ alert: null }), 5000);
+	};
 
 	render() {
 		return (
-			<div className='App'>
-				<NavBar />
-				<div className='container'>
-					<Alert alert={this.state.alert}/>
-					<Filter filterUsers={this.filterUsers} clearFilteredUsers={this.clearFilteredUsers} setAlert={this.setAlert}/>
-					<Users loading={this.state.loading} users={this.state.users} />
+			<Router>
+				<div className='App'>
+					<NavBar />
+					<div className='container'>
+						<Alert alert={this.state.alert} />
+						<Switch>
+							<Route
+								exact
+								path='/'
+								render={props => (
+									<Fragment>
+										<Filter
+											filterUsers={this.filterUsers}
+											clearFilteredUsers={this.clearFilteredUsers}
+											setAlert={this.setAlert}
+										/>
+										<Users
+											loading={this.state.loading}
+											users={this.state.users}
+										/>
+									</Fragment>
+								)}
+							/>
+							<Route exact path='/about' component={About}/>
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</Router>
 		);
 	}
 }
