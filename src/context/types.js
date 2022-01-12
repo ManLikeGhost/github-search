16a0b31328aefc88707c7b6c38b6e83ffc2f6ecf
manlikeghost <ekpoabasiama@gmail.com>
2022-01-12 02:05:25 +0100

    setting up context

---
+++ src/context/types.js
@@ -0,0 +1,7 @@
+export const FILTER_USERS = 'FILTER_USERS';
+export const GET_SINGLE_USER = 'GET_SINGLE_USER';
+export const GET_USER_REPOS = 'GET_USER_REPOS';
+export const CLEAR_FILTERED_USERS = 'CLEAR_FILTERED_USERS';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
